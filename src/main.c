7a3e93fe18ba8ffdb76d8ca3810b3075e228c852
bonskpy <bonskpy@gmail.com>
2026-02-22 18:31:20 +0100

    @ character is on the screen, quit command, move commands implemented

--- src/main.c
+++ src/main.c
@@ -2,15 +2,50 @@
 
 int main(void){
 
+  int pos_y, pos_x;
+  char ch, input;
+
+  pos_y = pos_x = 0;
+
   initscr();
   noecho();
   curs_set(0);
-
-  printw("Rogue0 - press any key to exit");
-
   wrefresh(stdscr);
 
-  getch();
+  pos_y = COLS / 2;
+  pos_x = LINES / 2;
+  ch = '@';
+
+  printw("Rogue0\n");
+  printw("Player position: %d,%d", pos_y, pos_x);
+
+
+  mvaddch(pos_y, pos_x, ch);
+
+  while((input = getch()) != 'q'){
+    
+    switch(input){
+      case 'h':
+        pos_x -= 1;
+        break;
+      case 'l':
+        pos_x += 1;
+        break;
+      case 'j':
+        pos_y += 1;
+        break;
+      case 'k':
+        pos_y -= 1;
+        break;
+    }
+
+    clear();
+    
+    mvaddch(pos_y, pos_x, ch);
+
+    wrefresh(stdscr);
+
+  }
 
   endwin();
 
