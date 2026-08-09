963978a4ba65e9db446933375117c43469e8acbe
bonskpy <bonskpy@gmail.com>
2026-08-09 22:50:33 +0200

    added entity module with a class to represent entities; handler is refacored to use Protocol approach instead of visitor pattern.

---
+++ src/input_handlers.py
@@ -0,0 +1,58 @@
+from typing import (  # Optional is a type annotation for types that can be None
+    Optional,
+    Protocol,
+)
+
+import tcod.event  # I am going to use only event module
+
+from actions import Action, EscapeAction, MovementAction
+
+
+# This defines contract. Any class implementing on_event() is a Handler.
+class Handler(Protocol):
+    def on_event(self, event: tcod.event.Event) -> Optional["Handler"]: ...
+
+
+class MainHandler:
+    def on_event(self, event: tcod.event.Event) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        match event:
+            case tcod.event.Quit():
+                raise SystemExit
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.UP):
+                action = MovementAction(dx=0, dy=-1)
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.DOWN):
+                action = MovementAction(dx=0, dy=1)
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.LEFT):
+                action = MovementAction(dx=-1, dy=0)
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.RIGHT):
+                action = MovementAction(dx=1, dy=0)
+            case tcod.event.Quit():
+                action = EscapeAction()
+
+        return action
+
+
+# class EventHandler(tcod.event.EventDispatch[Action]):
+#     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+#         raise SystemExit()
+
+#     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+#         action: Optional[Action] = None
+
+#         key = event.sym
+
+#         if key == tcod.event.K_UP:
+#             action = MovementAction(dx=0, dy=1)
+#         if key == tcod.event.K_DOWN:
+#             action = MovementAction(dx=0, dy=-1)
+#         if key == tcod.event.K_LEFT:
+#             action = MovementAction(dx=-1, dy=0)
+#         if key == tcod.event.K_RIGHT:
+#             action = MovementAction(dx=1, dy=0)
+
+#         if key == tcod.event.Quit:
+#             action = EscapeAction()
+
+#         return action
--- src/main.py
+++ src/main.py
@@ -1,8 +1,51 @@
 import tcod
 
+from actions import MovementAction
+from input_handlers import MainHandler
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 80
+
+# Typecasting is just passing variable to a constructor of chosed data type.
+# In case of custom classes a dudermethod has to be prepared. Casting float to int
+# will turncate return value. Casting string decimal to int throws error. It is good
+# practice to use defensive casting (try/except) and return ValueError on failure.
+
+player_x = int(SCREEN_WIDTH / 2)
+player_y = int(SCREEN_HEIGHT / 2)
+
 
 def main():
     print("Hello from rogue0!")
+
+    player_x = int(SCREEN_WIDTH / 2)
+    player_y = int(SCREEN_HEIGHT / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    handler = MainHandler()
+
+    with tcod.context.new(
+        columns=SCREEN_WIDTH, rows=SCREEN_HEIGHT, tileset=tileset, title="rogue0"
+    ) as context:
+        root_console = tcod.console.Console(
+            width=SCREEN_WIDTH, height=SCREEN_HEIGHT, order="F"
+        )
+        while True:
+            root_console.print(x=player_x, y=player_y, text="@")
+            context.present(root_console)
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = handler.on_event(event)
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+                else:
+                    continue
 
 
 if __name__ == "__main__":
